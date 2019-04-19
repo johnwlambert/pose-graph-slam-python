a9ca6da473489d9cf8397017f032c96956bc8483
John Lambert-Admin <jlambert-admin@ip-10-64-8-212.us-east-2.compute.internal>
2019-04-18 23:34:50 -0400

    change to classes for c++

--- se2_utils.py
+++ se2_utils.py
@@ -2,49 +2,71 @@
 import numpy as np
 import pdb
 
-def v2t(v):
-	"""
-	Computes the homogeneous transform matrix A of the pose vector v.
-		Args:
-		-	v: (3,1) vector
-		Returns:
-		-	A
-	"""
-	v = v.squeeze()
-	c = np.cos(v[2])
-	s = np.sin(v[2])
-	A = np.array([	[c, -s, v[0] ],
-					[s,  c, v[1] ],
-					[0., 0., 1.  ]])
-	return A
 
 
-def t2v(A):
-	"""
-	Computes the pose vector v from a homogeneous transform A.
-		Args:
-		-	A
-		Returns:
-		-	v
-	"""
-	v = np.zeros(3)
-	theta = np.arctan2(A[1,0],A[0,0])
-	v[:2] = A[:2,2]
-	v[2] = theta
-	return v.reshape(3,1)
+class SE2(object):
+	def __init__(self, R, t):
+		"""
+			Args:
+			-	R: Numpy array of shape (2,2)
+			-	t: Numpy array of shape (2,1)
+		"""
+		assert(R.shape == (2,2))
+		assert(t.shape == (2,))
+		self.R = R
+		self.t = t
 
-def invt(m):
-	"""
-	Inverts a homogenous transform.
-		Args:
-		-	m
-		Returns:
-		-	A
-	"""
-	R = m[:2,:2]
-	t = m[:2,2]
+		self.mat_3x3 = np.eye(3)
+		self.mat_3x3[:2,:2] = self.R
+		self.mat_3x3[:2,2] = self.t
 
-	A = np.eye(3)
-	A[:2,:2] = R.T
-	A[:2,2] = -R.T.dot(t)
-	return A
+	def inverse(self):
+		"""
+		Inverts a homogenous transform.
+			Args:
+			-	None
+
+			Returns:
+			-	T_inv: 3x3 Numpy array
+		"""
+		T_inv = np.eye(3)
+		T_inv[:2,:2] = self.R.T
+		T_inv[:2,2] = -self.R.T.dot(self.t)
+		return T_inv
+
+	def as_pose_vector(self):
+		"""
+		Computes the pose vector v from a homogeneous transform A.
+			Args:
+			-	A: 3x3 Numpy array
+			Returns:
+			-	v
+		"""
+		v = np.zeros(3)
+		v[:2] = self.t
+		theta = np.arctan2(self.R[1,0],self.R[0,0])
+		v[2] = theta
+		return v.reshape(3,1)
+
+
+class PoseVector(object):
+	def __init__(self, v):
+		v = v.squeeze()
+		self.v = v
+		self.t = v[:2]
+		self.theta = v[2]
+		
+	def as_SE2(self):
+		"""
+		Computes the homogeneous transform matrix A of the pose vector v.
+			Args:
+			-	v: (3,1) vector
+			Returns:
+			-	A: 3x3 Numpy array
+		"""
+		c = np.cos(self.theta)
+		s = np.sin(self.theta)
+		R = np.array([	[c, -s],
+						[s,  c]])
+		return SE2(R=R, t=self.t)
+
